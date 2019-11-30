d06a6f7c53c2752040f3a6dd876ca995d98d8182
HAnW97 <szzhw3166@163.com>
2019-11-30 16:37:38 +0800

    code

--- linear/trainLinear.py
+++ linear/trainLinear.py
@@ -1,86 +1,72 @@
-import datetime
-
-import numpy as np
 import pandas as pd
-from sklearn import datasets, linear_model
-from sklearn.decomposition import PCA
-from sklearn.externals import joblib
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn import linear_model
+from sklearn.decomposition import PCA, TruncatedSVD
 from sklearn.model_selection import train_test_split, KFold
 from sklearn.model_selection import cross_val_score
-import matplotlib.pyplot as plt
 import dataprocessing
 
-# training data path
-TRAIN_DATA_PATH = "train.csv"
-TEST_DATA_PATH = "test.csv"
-# preprocess both train and test data
+
+TRAIN_DATA_PATH = "../train.csv"
+TEST_DATA_PATH = "../test.csv"
+
+# load data
 data_train = pd.read_csv(TRAIN_DATA_PATH, sep=',')
 data_test = pd.read_csv(TEST_DATA_PATH, sep=',')
 
-data_train = dataprocessing.preprocessing_prediction(data_train)
-data_test = dataprocessing.preprocessing_prediction(data_test)
+# preprocess training data
+data_train = dataprocessing.dropOutliers(data_train)
+data_train = dataprocessing.preprocessing(data_train)
+train_shape = data_train.shape[0]  # nums of row
 
+# preprocess testing data
+data_test = dataprocessing.preprocessing(data_test)
+test_shape = data_test.shape[0]
 
-# drop_columns = dataprocessing.correlation(process_train)
-# print(drop_columns)
-
+# concat train.csv and test.csv together to do one-hot, ensure they have same columns
 dataset = pd.concat([data_train, data_test])
 dataset = dataprocessing.preprocessing_onehot(dataset)
 dataset.reset_index(drop=True, inplace=True)
 
-dataset.to_csv('aaa.csv')
-#dataset.drop(labels=drop_columns, axis=1, inplace=True)
-
-columns = list(dataset)
-columns.remove('playtime_forever')
-predictors = columns
+# drop columns of low correlation
+# corr_train = dataprocessing.preprocessing_onehot(data_train)
+# drop_columns = dataprocessing.correlation(corr_train)
+# drop_columns = dataprocessing.dropColumns(dataset)
+# dataset.drop(labels=drop_columns, axis=1, inplace=True)
 
 # split train and test data after preprocessing
-df_train = dataset.head(356)
-# t = dataprocessing.correlation(df_train)
-# t.to_csv('corr.csv')
+df_train = dataset.head(train_shape)
+df_test = dataset.tail(test_shape)
 
-df_test = dataset.tail(90)
-
+# get predictors
+predictors = list(dataset)
+predictors.remove('playtime_forever')
 X_train = df_train[predictors]
-pca = PCA(n_components=90)
-#X_train = pca.fit_transform(X_train)
 
-#print(pca.explained_variance_ratio_)
+# pca = PCA(n_components=90)
+# X_train = pca.fit_transform(X_train)
 
 y_train = df_train['playtime_forever']
-#X_train, X_test, y_train, y_test = train_test_split(df_train[predictors], df_train['playtime_forever'], test_size=0.34, random_state=42)
+
+# train linear model with L2 regularization
 LR = linear_model.Ridge().fit(X_train,y_train)
-#LR = linear_model.LinearRegression().fit(X_train, y_train)
+
+# kfold validation
 n_folds = 3
 kf = KFold(n_folds, shuffle=True, random_state=42).get_n_splits(X_train)
 scores = cross_val_score(LR, X_train, y_train, scoring='mean_squared_error', cv = kf)
 print(scores)
-# X_train2, X_test2, y_train2, y_test2 = train_test_split(X_train, y_train, test_size=0.34, random_state=42)
-# #scores = cross_val_score(LR, X_test, y_test, cv=1)
-#
-# print('Root Mean squared error: %.3f' % mean_squared_error(y_test2, LR.predict(X_test2))**0.5)##((y_test-LR.predict(X_test))**2).mean()
-# print('Variance score: %.3f' % r2_score(y_test2,LR.predict(X_test2)))#1-((y_test-LR.predict(X_test))**2).sum()/((y_test - y_test.mean())**2).sum()
-# print('score: %.3f' % LR.score(X_test,y_test))
-# plt.scatter(X_test, y_test, color='green')
-# plt.plot(X_test, LR.predict(X_test), color='red', linewidth=3)
-# plt.show()
-#joblib.dump(LR, "%s_linear_train_model.model" % (datetime.datetime.today().strftime("%Y%m%d")))
 
-#output test playtime_forever
+# predict
 X_test = df_test[predictors]
 #X_test = pca.fit_transform(X_test)
-#print(pca.explained_variance_ratio_)
 output = LR.predict(X_test)
-result = pd.DataFrame(output)
 
+# output result to submission.csv
+result = pd.DataFrame(output)
+result.reset_index(drop=True,inplace=True)
+result.index.name = 'id'
 result.rename(columns={0:'playtime_forever'}, inplace=True)
 
 result[result['playtime_forever']<0] = 0
 print(result)
 result.to_csv("submission_ridge.csv")
-
-#print(df_test)
-#r = result['playtime_forever']
-#r.to_csv("submission.csv")
--- xgboost/trainXgboost.py
+++ xgboost/trainXgboost.py
@@ -1,91 +1,75 @@
-
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.cross_validation import train_test_split
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import KFold
 import pandas as pd
-import matplotlib.pyplot as plt
 import xgboost as xgb
-import numpy as np
+
 import dataprocessing
 
 
-# training data path
-TRAIN_DATA_PATH = "train.csv"
-TEST_DATA_PATH = "test.csv"
-# preprocess both train and test data
+TRAIN_DATA_PATH = "../train.csv"
+TEST_DATA_PATH = "../test.csv"
+
+# load data
 data_train = pd.read_csv(TRAIN_DATA_PATH, sep=',')
 data_test = pd.read_csv(TEST_DATA_PATH, sep=',')
 
-#data_train = dataprocessing.dropNaN(data_train)
-#data_train = dataprocessing.dropOutliers(data_train)
-data_train = dataprocessing.preprocessing_prediction(data_train)
-train_shape = data_train.shape[0]
+# preprocess training data
+data_train = dataprocessing.preprocessing(data_train)
+train_shape = data_train.shape[0]  # nums of row
 
-data_test = dataprocessing.preprocessing_prediction(data_test)
+# preprocess testing data
+data_test = dataprocessing.preprocessing(data_test)
 test_shape = data_test.shape[0]
 
-# drop_columns = dataprocessing.correlation(process_train)
-# print(drop_columns)
-
+# concat train.csv and test.csv together to do one-hot, ensure they have same columns
 dataset = pd.concat([data_train, data_test])
 dataset = dataprocessing.preprocessing_onehot(dataset)
-# dataset.drop(labels=['genres','categories','tags'], axis=1, inplace=True)
 dataset.reset_index(drop=True, inplace=True)
 
-#drop_columns = dataprocessing.dropColumns(dataset)
-#dataset.drop(labels=drop_columns, axis=1, inplace=True)
-
-columns = list(dataset)
-columns.remove('playtime_forever')
-predictors = columns
+# drop columns of low correlation
+# corr_train = dataprocessing.preprocessing_onehot(data_train)
+# drop_columns = dataprocessing.correlation(corr_train)
+# drop_columns = dataprocessing.dropColumns(dataset)
+# dataset.drop(labels=drop_columns, axis=1, inplace=True)
 
 # split train and test data after preprocessing
 df_train = dataset.head(train_shape)
-# t = dataprocessing.correlation(df_train)
-# t.to_csv('corr.csv')
-
 df_test = dataset.tail(test_shape)
 
+# get predictors
+predictors = list(dataset)
+predictors.remove('playtime_forever')
 X_train = df_train[predictors]
 
-#pca = PCA(n_components=200)
-#X_train = pca.fit_transform(X_train)
-#print(pca.explained_variance_ratio_)
+# pca = PCA(n_components=90)
+# X_train = pca.fit_transform(X_train)
+
 y_train = df_train['playtime_forever']
 
-#forest = DecisionTreeRegressor(max_depth=100, max_features=0.6, min_samples_leaf=3, random_state=42, presort=True)
-#forest = AdaBoostRegressor(n_estimators=50)
-
-#forest = RandomForestRegressor(n_estimators=100,criterion="mse",random_state=42,min_samples_leaf=13, max_features=0.5, n_jobs=-1, oob_score=True)
-#model = xgb.XGBRegressor(max_depth=6, learning_rate=0.1, n_estimators=160, silent=False, reg_alpha=0, reg_lambda=1)
-#model = xgb.XGBRegressor(max_depth=6, learning_rate=0.1, n_estimators=150, subsample=0.8, reg_alpha=0, reg_lambda=1, silent=False)
-model = xgb.XGBRegressor(max_depth=7, learning_rate=0.1, n_estimators=180, subsample=0.8, silent=False, reg_alpha=0, objective='reg:gamma')
-model.fit(X_train,y_train)
+# train xgboost model
+xgb_model = xgb.XGBRegressor(max_depth=7, learning_rate=0.1, n_estimators=100, subsample=0.8, silent=False, reg_alpha=0, objective='reg:gamma')
+xgb_model.fit(X_train, y_train)
 
 
-#X_train2, X_test2, y_train2, y_test2 = train_test_split(X_train, y_train, test_size=0.34, random_state=0)
-# n_folds = 3
-# kf = KFold(n_folds, shuffle=True, random_state=42).get_n_splits(X_train)
-# scores = cross_val_score(model, X_train, y_train, scoring='mean_squared_error', cv = kf)
-# print(scores)
-#print('Root Mean squared error: %.3f' % mean_squared_error(y_test2, svm.predict(X_test2))**0.5)##((y_test-LR.predict(X_test))**2).mean()
-#print('Variance score: %.3f' % r2_score(y_test2,svm.predict(X_test2)))#1-((y_test-LR.predict(X_test))**2).sum()/((y_test - y_test.mean())**2).sum()
+# k-fold validation
+n_folds = 5
+kf = KFold(n_folds, shuffle=True, random_state=42).get_n_splits(X_train)
+scores = cross_val_score(xgb_model, X_train, y_train, scoring='mean_squared_error', cv = kf)
+print(scores)
 
+# predict
 X_test = df_test[predictors]
-#X_test = svd.fit_transform(X_test)
 #X_test = pca.fit_transform(X_test)
-#print(pca.explained_variance_ratio_)
-output = model.predict(X_test)
+output = xgb_model.predict(X_test)
+
+# output result to submission.csv
 result = pd.DataFrame(output)
 result.reset_index(drop=True,inplace=True)
 result.index.name = 'id'
-
 result.rename(columns={0:'playtime_forever'}, inplace=True)
 
 result[result['playtime_forever']<0] = 0
 print(result)
+
 result.to_csv("submission_xgb.csv")
